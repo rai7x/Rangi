857a206915511595ede6a2c208dc1bee6c5d61dd
rollierivera <72164424+rollierivera@users.noreply.github.com>
2021-10-17 17:37:32 -0400

    database implementation

--- app/src/main/java/com/example/rangi/DatabaseHelper.java
+++ app/src/main/java/com/example/rangi/DatabaseHelper.java
@@ -1,11 +1,19 @@
 package com.example.rangi;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 import android.content.Context;
 import android.content.ContentValues;
 import android.graphics.Color;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "TABLE_NAME";
@@ -23,7 +31,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String create_Table_SQL_Statement= "CREATE TABLE "
                 + TABLE_NAME + " ("
-                + COLUMN_ZERO_RGB + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_ZERO_RGB + " INTEGER, "
                 + COLUMN_ONE_NAME + " TEXT)";
 
         sqLiteDatabase.execSQL(create_Table_SQL_Statement);
@@ -34,11 +42,59 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
-    public long add_entry_to_DB (Color col) {
-        long rowID=10000;
+    public long add_entry_to_DB (ColourObj col) {
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues content_values = new ContentValues();
+        content_values.put(COLUMN_ZERO_RGB, col.getRGB());
+        content_values.put(COLUMN_ONE_NAME, col.getName());
+        long rowID = database.insert(TABLE_NAME, null, content_values);
+        database.close();
         return rowID;
     }
+    public ArrayList<ColourObj> get_all_from_DB () {
 
-}
+        ArrayList<ColourObj> returnList = new ArrayList<>();
+        String SQLite_query = "SELECT * FROM " + TABLE_NAME;
+
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery(SQLite_query, null);
+        if (cursor.moveToFirst()) {
+
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                int rgbVal = Integer.parseInt(cursor.getString(2));
+
+
+                ColourObj colourReturn = new ColourObj(name, rgbVal);
+
+                returnList.add(colourReturn);
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        database.close();
+        return returnList;
+    }
+    public ColourObj get(ColourObj col) {
+
+        ArrayList<ColourObj> returnList = new ArrayList<>();
+        String SQLite_query = "SELECT * FROM " + TABLE_NAME+ "WHERE COLUMN_ZERO_RGB=="+String.valueOf(col.getRGB());
+        ColourObj colourReturn=new ColourObj();
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery(SQLite_query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                int rgbVal = Integer.parseInt(cursor.getString(2));
+                colourReturn.setName(name);
+                colourReturn.setRGB(rgbVal);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        database.close();
+        return colourReturn;
+    }
+
+    }
--- app/src/main/java/com/example/rangi/MainActivity.java
+++ app/src/main/java/com/example/rangi/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.rangi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
+    private void chooseColor() {
+
+    }
+
 }
