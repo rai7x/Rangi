34fe6d20a37bc23c0c9168bf9d60a82751ad7432
raulriverarojas <72164424+raulriverarojas@users.noreply.github.com>
2021-10-17 17:56:54 -0400

    Revert "Raul"

--- app/src/main/java/com/example/rangi/MainActivity.java
+++ app/src/main/java/com/example/rangi/MainActivity.java
@@ -2,22 +2,18 @@ package com.example.rangi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
-
 
     public void startFavourites(View view) {
         Intent intent = new Intent(this, favouriteColours.class);
@@ -28,5 +24,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, colourSelection.class);
         startActivity(intent);
     }
-
 }
